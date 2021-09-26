28d1985210fbdd15b65ddfc889877148767f4d3a
mhk-local <superuser.mhk@gmail.com>
2021-09-26 07:54:31 +0300

    Install and config nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Hello !!!')
 });
 
 const port = process.env.PORT || 3000; 
