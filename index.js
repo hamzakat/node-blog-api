da25be19b713913efd69f4689a334d72745aaf04
mhk-local <superuser.mhk@gmail.com>
2021-10-02 04:26:56 +0300

    Configuring MongoDB, creating a named anonymous volume for the DB, and managing the required env. vars.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config.js/config');
 const express = require('express')
 
-const app = express()
+const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("connected"))
+    .catch((e) => console.log(e))
 
 app.get('/', (req, res) => {
     res.send('Hello !!!')
@@ -8,4 +20,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000; 
 
-app.listen(port, () => console.log(`listening on ${port}`))
+app.listen(port, () => console.log(`listening on ${port}`)) 
